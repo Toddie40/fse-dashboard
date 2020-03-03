2e782fe5c6c70dccb911f2fd3e3d79e7fe99804e
Alex Todd <alex.todd40@gmail.com>
2020-03-03 16:39:25 +0000

    added a skeleton module for merging flow

---
+++ src/components/mergingflow.component.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Module from './module.component';
+
+export default class MergingFlow extends Module{
+
+    constructor(props){
+        super(props)
+        this.name = "Merging Flow"
+    }
+
+
+    
+
+
+
+    renderBody(){
+        return (
+            <div>Merging Flow calculator coming soon</div>
+        )
+    }
+}
