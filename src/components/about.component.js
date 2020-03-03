7129248752972ffe478c9edaf7c33047c477d650
Alex Todd <alex.todd40@gmail.com>
2020-03-03 10:29:00 +0000

    Updated about page

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -11,7 +11,19 @@ export default class About extends Component{
     }
     render(){
         return (
-            <h2>{this.name}</h2>
+            <div className="container">
+                <div className="card">
+                    <h2 className="card-header">{this.name}</h2>
+                    <div className="card-body">
+                        <h5>Fire Safety Engineering Dashboard by OFR</h5>
+                        <p>A collection of fire safety engineering tools for routine calculations to help with speeding up the process of writing fire strategies</p>
+                    </div>        
+                    <div className="card-footer">Alex Todd | OFR Consultants</div>
+                </div>
+
+            </div>
+            
+
         )
     }
 }
