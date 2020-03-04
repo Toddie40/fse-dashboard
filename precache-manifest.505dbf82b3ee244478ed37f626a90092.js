53e7cfc6d2d7b4072eabaa0db7ec89ef1892da96
Alex Todd <alex.todd40@gmail.com>
2020-03-04 15:59:37 +0000

    Updates

--- precache-manifest.505dbf82b3ee244478ed37f626a90092.js
+++ precache-manifest.505dbf82b3ee244478ed37f626a90092.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "72eb42d080d5a0735ac2a862f25494bf",
+    "revision": "12ace6f3d8b004765ebfe22a498f9fd8",
     "url": "/fse-dashboard/index.html"
   },
   {
-    "revision": "ef71224f120fe9ecd95e",
-    "url": "/fse-dashboard/static/css/main.d0fa6ed3.chunk.css"
+    "revision": "eb2e4d8ca6c51dfee919",
+    "url": "/fse-dashboard/static/css/main.22345c69.chunk.css"
   },
   {
     "revision": "c472d5907b28a8b7346f",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fse-dashboard/static/js/2.95c7c003.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ef71224f120fe9ecd95e",
-    "url": "/fse-dashboard/static/js/main.fca38eb8.chunk.js"
+    "revision": "eb2e4d8ca6c51dfee919",
+    "url": "/fse-dashboard/static/js/main.6880d12c.chunk.js"
   },
   {
     "revision": "646fbdaecf4bb436c4bc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fse-dashboard/precache-manifest.dba6f2be5f137600cab681c868d73408.js"
+  "/fse-dashboard/precache-manifest.505dbf82b3ee244478ed37f626a90092.js"
 );
 
 self.addEventListener('message', (event) => {
