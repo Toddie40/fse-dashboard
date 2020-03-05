e5d05ecff52a0af8dbc0e5863184388b28e7000e
Alex Todd <alex.todd40@gmail.com>
2020-03-05 15:25:20 +0000

    Added logo to module headers

--- src/components/module.component.js
+++ src/components/module.component.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import logo from './../logo.svg';
 
 export default class Module extends Component {
     constructor(props){
         super(props);
+        this.logo = logo;
+        this.logostyle = { 
+            height: '3rem',
+            float: 'right'
+            } 
+        this.logosrc = "https://images.squarespace-cdn.com/content/v1/5a61f9539f8dceafed8c0aea/1538391094315-DMAJ4KSB8F3BB7YIYAD3/ke17ZwdGBToddI8pDm48kL1PC3gM8Rd6YHHCa9IyTboUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2dtz9D7BxKE7nEGUPCO0e-x3MMDjV9uhX05eW5661eHWZCjLISwBs8eEdxAxTptZAUg/OFR-LtGrey+copy%402x.png?format=500w"
         this.title="moduleTitle"
         this.footer=""
     }
@@ -20,7 +27,16 @@ export default class Module extends Component {
             <div className="container">
                 <div className="card shadow"> 
                     <div className="card-header">
-                        <h1>{this.title}</h1>
+                        <div className="d-flex">
+                            <h1 class="col-md justify-content-center">
+                            {this.title}
+                            </h1>
+                            <div className="col-md ml-auto justify-content-right" >
+                                <img
+                                    style={this.logostyle}
+                                    src={this.logosrc}/>
+                            </div>
+                        </div>
                     </div>
                     <div className="card-body">
                         {this.renderBody()}
