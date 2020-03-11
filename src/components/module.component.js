7967bb8cec089e0bec46fcd434c3ea931fe347b6
Alex Todd <alex.todd40@gmail.com>
2020-03-11 12:02:37 +0000

    Styling updated for improved mobile experience

--- src/components/module.component.js
+++ src/components/module.component.js
@@ -6,7 +6,6 @@ export default class Module extends Component {
         super(props);
         this.logo = logo;
         this.logostyle = { 
-            height: '3rem',
             float: 'right'
             } 
         this.logosrc = "https://images.squarespace-cdn.com/content/v1/5a61f9539f8dceafed8c0aea/1538391094315-DMAJ4KSB8F3BB7YIYAD3/ke17ZwdGBToddI8pDm48kL1PC3gM8Rd6YHHCa9IyTboUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2dtz9D7BxKE7nEGUPCO0e-x3MMDjV9uhX05eW5661eHWZCjLISwBs8eEdxAxTptZAUg/OFR-LtGrey+copy%402x.png?format=500w"
@@ -28,11 +27,12 @@ export default class Module extends Component {
                 <div className="card shadow"> 
                     <div className="card-header">
                         <div className="d-flex">
-                            <h1 className="col-md justify-content-center">
+                            <h1 className="module-header-title col-md justify-content-center">
                             {this.title}
                             </h1>
                             <div className="col-md ml-auto justify-content-right" >
                                 <img
+                                    className="module-header-logo"
                                     style={this.logostyle}
                                     src={this.logosrc}/>
                             </div>
